2f84a5a46235fddcf47df607391502a3e1306b2f
stoarch <stormarchitextor@gmail.com>
2018-06-03 20:12:38 +0600

    Cell press handling introduced

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Cell : MonoBehaviour, IPointerDownHandler {
+
+    public enum CellState
+    {
+        Off,
+        On
+    }
+
+    [SerializeField]
+    Image image;
+
+    [SerializeField]
+    int row = 0;
+    [SerializeField]
+    int col = 0;
+
+    [SerializeField]
+    Game game;
+
+    [SerializeField]
+    Sprite offSprite;
+    [SerializeField]
+    Sprite playerOneSprite;
+    [SerializeField]
+    Sprite playerTwoSprite;
+
+    [SerializeField]
+    CellState state;
+
+    [SerializeField]
+    Game.Player owner;
+
+	void Start ()
+    {
+		
+	}
+
+
+    CellState State
+    {
+        get
+        {
+            return state;
+        }
+        set
+        {
+            state = value;
+            UpdateCellView();
+        }
+    }
+
+    private void UpdateCellView()
+    {
+        if(image == null)
+        {
+            Debug.LogWarning("No image set for cell");
+            return;
+        }
+
+        switch (state)
+        {
+            case CellState.Off:
+                image.sprite = offSprite; break;
+            case CellState.On:
+                SetPlayerSprite();  break;
+            default:
+                break;
+        }
+    }
+
+    private void SetPlayerSprite()
+    {
+        switch (owner)
+        {
+            case Game.Player.FirstPlayer:
+                image.sprite = playerOneSprite; break;
+            case Game.Player.SecondPlayer:
+                image.sprite = playerTwoSprite;  break;
+            default:
+                break;
+        }
+    }
+
+    private void InvertCellState()
+    {
+        switch (State)
+        {
+            case CellState.Off:
+                State = CellState.On;  break;
+            case CellState.On:
+                State = CellState.Off; break;
+            default:
+                break;
+        }
+    }
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        owner = game.CurrentPlayer;
+        State = CellState.On;
+        game.PressedCell(this);
+    }
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour {
+
+    public enum Player
+    {
+        FirstPlayer,
+        SecondPlayer
+    }
+
+    [SerializeField]
+    Toggle playerOneTurnView;
+    [SerializeField]
+    Toggle playerTwoTurnView;
+
+    Player currentPlayer;
+    public Player CurrentPlayer
+    {
+        get
+        {
+            return currentPlayer;
+        }
+    }
+
+    public void SetPlayerOneTurn()
+    {
+        currentPlayer = Player.FirstPlayer;
+    }
+
+    public void SetPlayerTwoTurn()
+    {
+        currentPlayer = Player.SecondPlayer;
+    }
+
+    internal void PressedCell(Cell cell)
+    {
+    }
+}
